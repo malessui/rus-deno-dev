acb1b0f20c6ddba662190b142918e6884d26956f
muhrusdi <mhmrofficial@gmail.com>
2021-06-23 20:21:07 +0800

    remove comment

--- index.tsx
+++ index.tsx
@@ -2,11 +2,7 @@ import {React, react, ReactDOM} from './deps.ts';
 import App from './App.tsx';
 
 addEventListener("fetch", (event) => {
-  const html = ReactDOM.renderToString(
-    <react.StrictMode>
-      <App />
-    </react.StrictMode>
-  );
+  const html = ReactDOM.renderToString(<App />);
   event.respondWith(
     new Response(html, {
       headers: { "content-type": "text/html; charset=utf-8" },
