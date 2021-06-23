e67ab6f317a87afa58f172cfb622973e89a517c6
muhrusdi <mhmrofficial@gmail.com>
2021-06-23 20:25:20 +0800

    preact

--- index.tsx
+++ index.tsx
@@ -1,11 +1,21 @@
-import {React, react, ReactDOM} from './deps.ts';
-import App from './App.tsx';
+import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
+import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
+
+function App() {
+  return (
+    <html>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>
+  );
+}
 
 addEventListener("fetch", (event) => {
-  const html = ReactDOM.renderToString(<App />);
+  const html = renderToString(<App />);
   event.respondWith(
     new Response(html, {
       headers: { "content-type": "text/html; charset=utf-8" },
-    });
+    }),
   );
 });
